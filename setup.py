e5a7083fccfebb1131385c589b60528041b46556
ipgmvq <70766280+ipgmvq@users.noreply.github.com>
2023-04-03 01:55:14 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     packages = ['nodeweightedbudget'],
     version = '0.0.1',
     license = 'MIT',
-    description = 'This package maximizes the total prize with a budget constraint for an undirected node-weighted rooted graph very loosely following Bateni 2018 algorithm from DOI:10.1137/15M102695X',
+    description = 'This package maximizes the total prize with a budget constraint for an undirected node-weighted rooted graph very very loosely following Bateni 2018 algorithm from DOI:10.1137/15M102695X',
     url = 'https://github.com/ipgmvq/nodeweightedbudget',
     download_url = 'https://github.com/ipgmvq/nodeweightedbudget/archive/refs/tags/v0.0.1.tar.gz',
     keywords = ['nodeweightedbudget', 'graph', 'undirected', 'budget', 'nodeweighted'],
-    classifiers = [],
+    classifiers = ["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
     install_requires = ['numpy']
 )
